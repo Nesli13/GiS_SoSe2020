c79238ae9613dd97c4bce966c3c524d739109a53
Neslisah Koc <neslisah.koc@hs-furtwangen.de>
2020-05-29 09:44:26 +0200

    Aufgabe05

--- Aufgabe05/script.ts
+++ Aufgabe05/script.ts
@@ -1,104 +1,37 @@
+namespace Aufgabe05 {
+    for (let i: number = 0; i < süßigkeiten.length; i++) {
+        //Überschrift
+        let artikelHoch: HTMLElement = document.createElement("h4");
+        artikelHoch.innerHTML = "Gönn dir doch mal was Gutes!";
+        artikelHoch.setAttribute ("id", "Hoch");
 
-//Interface
-interface Süßigkeite {
-    img: string;
-    name: string;
-    preis: string;
-    beschreibung: string;
+        let artikelSüßwaren: HTMLElement = document.createElement("h1");
+        artikelSüßwaren.innerHTML = "Süßwaren";
+        artikelSüßwaren.setAttribute("id", "Süßwaren");
+
+        //Div
+        let divCandy: HTMLElement = document.createElement("div");
+        divCandy.setAttribute("class", "Produkte");
+
+        //Bild
+        let imgCandy: HTMLElement = document.createElement("img");
+        imgCandy.setAttribute("src", süßigkeiten[i].img);
+        imgCandy.setAttribute("alt", süßigkeiten[i].alt);
+        
+        //Name
+        let candyName: HTMLElement = document.createElement("h2");
+        candyName.innerHTML = süßigkeiten[i].name;
+
+        //Preis
+        let candyPreis: HTMLElement = document.createElement("h3");
+        candyPreis.innerHTML = süßigkeiten[i].preis;
+
+        //Beschreibung
+        let candybeschreibung: HTMLElement = document.createElement("p");
+        candybeschreibung.innerHTML = süßigkeiten[i].beschreibung;
+
+        //Button
+        /*let button: HTMLElement = document.createElement("button")*/
+        
+    }
 }
-//Candy
-let candy1: Süßigkeite = {
-    img: "candy1.jpeg", name: "Reese's Peanut Butter Cups 42g", preis: "0,99 €", beschreibung: "(2,36 €/100g)"
-};
-let candy2: Süßigkeite = {
-    img: "candy2.jpg", name: "m&m's Dark Chocolate 286,3g", preis: "8,99 €", beschreibung: "(31,40 €/1kg)"
-};
-let candy3: Süßigkeite = {
-    img: "candy3.jpg", name: "Jelly Belly Bean Boozled Refill Flip-Top-Box 45g", preis: "2,49 €", beschreibung: "(5,53 €/100g)"
-};
-let candy4: Süßigkeite = {
-    img: "candy4.jpg", name: "Oreo Choc White 246g", preis: "2,99 €", beschreibung: "(1,22 €/100g)"
-};
-let candy5: Süßigkeite = {
-    img: "candy5.jpg", name: "Hershey's Cookies'n'Creme 43g", preis: "0,99 €", beschreibung: "(2,30 €/100g)"
-};
-let candy6: Süßigkeite = {
-    img: "candy6.jpg", name: "Skittles Fruits 160g", preis: "1,19 €", beschreibung: "(0,74 €/100g)"
-};
-let candy7: Süßigkeite = {
-    img: "candy7.jpg", name: "Wonka Nerds Grape & Strawberry 46,7g", preis: "1,49 €", beschreibung: "(3,19 €/100g)"
-};
-let candy8: Süßigkeite = {
-    img: "candy8.jpg", name: "Barkleys Chocolate Mint 50g", preis: "1,99 €", beschreibung: "(3,98 €/100g)"
-};
-let candy9: Süßigkeite = {
-    img: "candy9.jpg", name: "Haribo Sauerier Sauer 150er", preis: "5,79 €", beschreibung: "(4,29 €/1kg)"
-};
-let candy10: Süßigkeite = {
-    img: "candy10.jpg", name: "nimm2 Lolly 12er", preis: "1,49 €", beschreibung: "(1,24 €/100g)"
-};
-let candy11: Süßigkeite = {
-    img: "candy11.jpg", name: "Choclait Chips Himbeer 115g", preis: "1,99 €", beschreibung: "(1,73 €/100g)"
-}; 
-let candy12: Süßigkeite = {
-    img: "candy12.jpg", name: "Yogurette 24er", preis: "3,19 €", beschreibung: "(10,63 €/1kg)"
-};
-//Array
-let süßigkeiten: Süßigkeite[] = [ candy1, candy2, candy3, candy4, candy5, candy6, candy7, candy8, candy9, candy10, candy11, candy12];
-
-
-//Interface
-interface Scharfes {
-    img: string;
-    name: string;
-    preis: string;
-    beschreibung: string;
-
-}
-
-//Scharf
-let scharff: Scharfes = {
-    img: "scharff.jpg", name: "Doritos Chilli Heatwave 70g", preis: "1,99 €", beschreibung: "(2,84 €/100g)"
-};
-let scharf2: Scharfes = {
-    img: "scharf2.jpg", name: "Sallos X-Plosiv 150g", preis: "0,99 €", beschreibung: "(0,66 €/100g)"
-};
-let scharf3: Scharfes = {
-    img: "scharf3.jpg", name: "Snyder's Jalapeño 125g", preis: "1,99 €", beschreibung: "(1,59 €/100g)"
-};
-let scharf4: Scharfes = {
-    img: "scharf4.jpg", name: "Chio Tortillas Hot Chili 125g", preis: "1,59 €", beschreibung: "(1,27 €/100g)"
-};
-let scharf5: Scharfes = {
-    img: "scharf5.jpg", name: "Khao Shong Wasabi Peanuts 140g", preis: "2,69 €", beschreibung: "(1,92 €/100g)"
-};
-let scharf6: Scharfes = {
-    img: "scharf6.jpg", name: "Lay's Bugles Sweet Chilli 100g", preis: "1,49 €", beschreibung: "(1,49 €/100g)"
-};
-let scharf7: Scharfes = {
-    img: "scharf7.jpg", name: "XOX Salsa Dip 300ml", preis: "1,99 €", beschreibung: "(6,63 €/1l)"
-};
-let scharf8: Scharfes = {
-    img: "scharf8.jpg", name: "Crunchips Roasted Chili & Grilled Cheese 150g ", preis: "1,69 €", beschreibung: "(1,13 €/100g)"
-};
-let scharf9: Scharfes = {
-    img: "scharf9.jpg", name: "Jimmy's Popcorn Sweet Chili BBQ with Tabasco 90g", preis: "1,99 €", beschreibung: "(2,21 €/100g)"
-};
-let scharf10: Scharfes = {
-    img: "scharf10.jpg", name: "Lindt Excellence Chili 100g", preis: "2,29 €", beschreibung: "(2,29 €/100g)"
-};
-let scharf11: Scharfes = {
-    img: "scharf11.jpg", name: "LPepper-King Habanero Popcorn 90g", preis: "2,19 €", beschreibung: "(2,43 €/100g)"
-};
-let scharf12: Scharfes = {
-    img: "scharf12.jpg", name: "NicNac's Hot 110g", preis: "1,49 €", beschreibung: "(1,35 €/100g)"
-};
-//Array
-let scharf: Scharfes[] = [scharff, scharf2, scharf3, scharf4, scharf5, scharf6, scharf7, scharf8, scharf9, scharf10, scharf11, scharf12];
-
-
-//Code
-/*for (let index: number = 0; index < süßigkeiten.length; index++) {
-    let Div: HTMLDivElement = document.createElement("div");
-    Div.id = "divNr" + index;
-}*/
