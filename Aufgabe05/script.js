1054c386a1e11e40513c5f25d4127aa55bc319c7
Neslisah Koc <neslisah.koc@hs-furtwangen.de>
2020-05-29 10:12:11 +0200

    Aufgabe05

--- Aufgabe05/script.js
+++ Aufgabe05/script.js
@@ -27,6 +27,44 @@ var Aufgabe05;
         candybeschreibung.innerHTML = süßigkeiten[i].beschreibung;
         //Button
         /*let button: HTMLElement = document.createElement("button")*/
+        // Alle Tags zu div Container
+        divCandy.appendChild(imgCandy);
+        divCandy.appendChild(candyName);
+        divCandy.appendChild(candyPreis);
+        divCandy.appendChild(candybeschreibung);
+        document.getElementById("liste")?.appendChild(artikelSüßwaren);
+        document.getElementById("liste")?.appendChild(divCandy);
+    }
+})(Aufgabe05 || (Aufgabe05 = {}));
+(function (Aufgabe05) {
+    for (let i = 0; i < scharf.length; i++) {
+        //Überschrift
+        let artikel = document.createElement("h1");
+        artikel.innerHTML = "Extra Scharf";
+        let artikelScharf = document.createElement("div");
+        artikelScharf.setAttribute("id", "ExtraScharf");
+        //Bild
+        let imgScharf = document.createElement("img");
+        imgScharf.setAttribute("src", scharf[i].img);
+        imgScharf.setAttribute("alt", scharf[i].alt);
+        //Name
+        let scharfName = document.createElement("h2");
+        scharfName.innerHTML = scharf[i].name;
+        //Preis
+        let scharfPreis = document.createElement("h3");
+        scharfPreis.innerHTML = scharf[i].preis;
+        //Beschreibung
+        let scharfbeschreibung = document.createElement("p");
+        scharfbeschreibung.innerHTML = scharf[i].beschreibung;
+        //Button
+        /*let button: HTMLElement = document.createElement("button")*/
+        // Alle Tags zu div Container
+        artikelScharf.appendChild(imgScharf);
+        artikelScharf.appendChild(scharfName);
+        artikelScharf.appendChild(scharfPreis);
+        artikelScharf.appendChild(scharfbeschreibung);
+        document.getElementById("liste")?.appendChild(artikel);
+        document.getElementById("liste")?.appendChild(artikelScharf);
     }
 })(Aufgabe05 || (Aufgabe05 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe05/script.ts
+++ Aufgabe05/script.ts
@@ -32,6 +32,54 @@ namespace Aufgabe05 {
 
         //Button
         /*let button: HTMLElement = document.createElement("button")*/
+
+        // Alle Tags zu div Container
+        divCandy.appendChild(imgCandy);
+        divCandy.appendChild(candyName);
+        divCandy.appendChild(candyPreis);
+        divCandy.appendChild(candybeschreibung);
+
+        document.getElementById("liste")?.appendChild(artikelSüßwaren);
+        document.getElementById("liste")?.appendChild(divCandy);
+    }
+}
+namespace Aufgabe05 {
+    for (let i: number = 0; i < scharf.length; i++) {
+        //Überschrift
+       
+        let artikel: HTMLElement = document.createElement("h1");
+        artikel.innerHTML = "Extra Scharf";
+
+        let artikelScharf: HTMLElement = document.createElement("div");
+        artikelScharf.setAttribute("id", "ExtraScharf");
+
+        //Bild
+        let imgScharf: HTMLElement = document.createElement("img");
+        imgScharf.setAttribute("src", scharf[i].img);
+        imgScharf.setAttribute("alt", scharf[i].alt);
         
+        //Name
+        let scharfName: HTMLElement = document.createElement("h2");
+        scharfName.innerHTML = scharf[i].name;
+
+        //Preis
+        let scharfPreis: HTMLElement = document.createElement("h3");
+        scharfPreis.innerHTML = scharf[i].preis;
+
+        //Beschreibung
+        let scharfbeschreibung: HTMLElement = document.createElement("p");
+        scharfbeschreibung.innerHTML = scharf[i].beschreibung;
+
+        //Button
+        /*let button: HTMLElement = document.createElement("button")*/
+
+        // Alle Tags zu div Container
+        artikelScharf.appendChild(imgScharf);
+        artikelScharf.appendChild(scharfName);
+        artikelScharf.appendChild(scharfPreis);
+        artikelScharf.appendChild(scharfbeschreibung);
+
+        document.getElementById("liste")?.appendChild(artikel);
+        document.getElementById("liste")?.appendChild(artikelScharf);
     }
 }
