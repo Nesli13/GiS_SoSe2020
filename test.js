59229bfa96d19b5a13cc5702e3137c88ac0e9b71
Neslisah Koc <neslisah.koc@hs-furtwangen.de>
2020-06-05 00:28:39 +0200

    test

---
+++ test.js
@@ -0,0 +1,9 @@
+"use strict";
+var L06_Load;
+(function (L06_Load) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        console.log(_event);
+    }
+})(L06_Load || (L06_Load = {}));
+//# sourceMappingURL=test.js.map
