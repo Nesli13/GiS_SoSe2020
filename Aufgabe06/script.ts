523613af676d005a4d621d6121b025db053578d8
Neslisah Koc <neslisah.koc@hs-furtwangen.de>
2020-06-07 18:05:51 +0200

    neu

--- Aufgabe06/script.ts
+++ Aufgabe06/script.ts
@@ -91,12 +91,12 @@
         scharfes.addEventListener("click", handleKatScharf);
 
         function handleKatCandys(_event: Event): void {
-        document.getElementById("#Süßwaren")?.setAttribute("style", "display: normal");
-        document.getElementById("#ExtraScharf")?.setAttribute("style", "display: none");
+        document.getElementById("süßwaren")?.setAttribute("style", "display: normal");
+        document.getElementById("extraScharf")?.setAttribute("style", "display: none");
      }
         function handleKatScharf(_evet: Event): void {
-            document.getElementById("#Süßwaren")?.setAttribute("style", "display: none");
-            document.getElementById("#ExtraScharf")?.setAttribute("style", "display: normal");     
+            document.getElementById("süßwaren")?.setAttribute("style", "display: none");
+            document.getElementById("extraScharf")?.setAttribute("style", "display: normal");     
            }
 }
 }
