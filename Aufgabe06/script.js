0977b2eb9989d276259afd27ea31411476a11237
Neslisah Koc <neslisah.koc@hs-furtwangen.de>
2020-06-07 19:26:23 +0200

    Neu

--- Aufgabe06/script.js
+++ Aufgabe06/script.js
@@ -1,120 +1,49 @@
 "use strict";
-var Aufgabe06;
-(function (Aufgabe06) {
-    let aktuelleProdukte = "";
-    function artikelErzeugen(süßigkeiten) {
-        //Div
-        let divCandy = document.createElement("div");
-        divCandy.setAttribute("class", "Produkte");
-        //Bild
-        let imgCandy = document.createElement("img");
-        imgCandy.setAttribute("src", süßigkeiten.img);
-        imgCandy.setAttribute("alt", süßigkeiten.alt);
-        //Name
-        let candyName = document.createElement("h2");
-        candyName.innerHTML = süßigkeiten.name;
-        //Preis
-        let candyPreis = document.createElement("h3");
-        candyPreis.innerHTML = süßigkeiten.preis + "€";
-        //Beschreibung
-        let candybeschreibung = document.createElement("p");
-        candybeschreibung.innerHTML = süßigkeiten.beschreibung;
-        //Button
-        let button = document.createElement("input");
-        button.type = "button";
-        button.value = "Kaufen";
-        // Alle Tags zu div Container
-        divCandy.appendChild(imgCandy);
-        divCandy.appendChild(candyName);
-        divCandy.appendChild(candyPreis);
-        divCandy.appendChild(candybeschreibung);
-        divCandy.appendChild(button);
-        switch (süßigkeiten.kategorie) {
-            case 1:
-                let getContainer1 = document.getElementById("kategorie1");
-                getContainer1.appendChild(divCandy);
-                break;
-            case 2:
-                let getContainer2 = document.getElementById("kategorie2");
-                getContainer2.appendChild(divCandy);
-                break;
-            default:
-                break;
-        }
-    }
-    function locationHashChanged() {
-        let produktListe = document.getElementById("kategorie1" + "kategorie2");
-        if (produktListe) {
-            produktListe.innerHTML = "";
-        }
-        /*if (location.hash) {
-            aktuelleProdukte = location.hash.split("#")[1];
-            if (aktuelleProdukte == "home") {
-                let artikelUeberschrift: HTMLElement = document.createElement("h2");
-                artikelUeberschrift.innerHTML = "Süßwaren und Extra Scharf";
-                document.getElementById("kategorie1" + "kategorie2")?.appendChild(artikelUeberschrift);
-            }
-            else {
-                let artikelUeberschrift: HTMLElement = document.createElement("h2");
-                artikelUeberschrift.innerHTML = aktuelleProdukte;
-                document.getElementById("kategorie1" + "kategorie2")?.appendChild(artikelUeberschrift);
-            }
-        }*/
-        for (let i = 0; i < Aufgabe06.süßigkeiten.length; i++) {
-            if (aktuelleProdukte == Aufgabe06.süßigkeiten[i].kategorie) {
-                artikelErzeugen(Aufgabe06.süßigkeiten[i]);
-            }
-            else if (aktuelleProdukte == "home") {
-                artikelErzeugen(Aufgabe06.süßigkeiten[i]);
+var Aufgabe05;
+(function (Aufgabe05) {
+    artikelErzeugen();
+    function artikelErzeugen() {
+        for (let i = 0; i < süßigkeiten.length; i++) {
+            //Div
+            let divCandy = document.createElement("div");
+            divCandy.setAttribute("class", "Produkte");
+            //Bild
+            let imgCandy = document.createElement("img");
+            imgCandy.setAttribute("src", süßigkeiten[i].img);
+            imgCandy.setAttribute("alt", süßigkeiten[i].alt);
+            //Name
+            let candyName = document.createElement("h2");
+            candyName.innerHTML = süßigkeiten[i].name;
+            //Preis
+            let candyPreis = document.createElement("h3");
+            candyPreis.innerHTML = süßigkeiten[i].preis + "€";
+            //Beschreibung
+            let candybeschreibung = document.createElement("p");
+            candybeschreibung.innerHTML = süßigkeiten[i].beschreibung;
+            //Button
+            let button = document.createElement("input");
+            button.type = "button";
+            button.value = "Kaufen";
+            candyPreis.appendChild(button);
+            // Alle Tags zu div Container
+            divCandy.appendChild(imgCandy);
+            divCandy.appendChild(candyName);
+            divCandy.appendChild(candyPreis);
+            divCandy.appendChild(candybeschreibung);
+            divCandy.appendChild(button);
+            switch (süßigkeiten[i].kategorie) {
+                case 1:
+                    let getContainer1 = document.getElementById("kategorie1");
+                    getContainer1.appendChild(divCandy);
+                    break;
+                case 2:
+                    let getContainer2 = document.getElementById("kategorie2");
+                    getContainer2.appendChild(divCandy);
+                    break;
+                default:
+                    break;
             }
         }
     }
-    function pageLoad() {
-        location.hash = "startseite";
-        aktuelleProdukte = "startseite";
-        // Artikelliste
-        if (location.hash) {
-            aktuelleProdukte = location.hash.split("#")[1];
-            if (aktuelleProdukte == "startseite") {
-                let artikelUeberschrift = document.createElement("h2");
-                artikelUeberschrift.innerHTML = "Frische Blumen und Vasen";
-                document.getElementById("artikelListe")?.appendChild(artikelUeberschrift);
-            }
-            else {
-                let artikelUeberschrift = document.createElement("h2");
-                artikelUeberschrift.innerHTML = aktuelleProdukte;
-                document.getElementById("artikelListe")?.appendChild(artikelUeberschrift);
-            }
-        }
-        for (let i = 0; i < Aufgabe06.süßigkeiten.length; i++) {
-            if (aktuelleProdukte == Aufgabe06.süßigkeiten[i].kategorien) {
-                artikelErzeugen(Aufgabe06.süßigkeiten[i]);
-            }
-            else if (aktuelleProdukte == "home") {
-                artikelErzeugen(Aufgabe06.süßigkeiten[i]);
-            }
-        }
-    }
-    window.onhashchange = locationHashChanged;
-    window.onload = pageLoad;
-    //let warenkorbTotal: number = 0;
-    let artikelZaehler = 0;
-    let warenkorbTotal = 0;
-    function handleClick(_event) {
-        artikelZaehler++;
-        if (artikelZaehler >= 0) {
-            const blase = document.getElementById("divBlase");
-            if (blase)
-                blase.innerHTML = "" + artikelZaehler;
-        }
-        let produktId = _event.currentTarget.parentElement.getAttribute("id");
-        // Summe der Preise  
-        for (let i = 0; i < Aufgabe06.süßigkeiten.length; i++) {
-            if (Aufgabe06.süßigkeiten[i].id == produktId) {
-                warenkorbTotal = warenkorbTotal + Aufgabe06.süßigkeiten[i].preis;
-            }
-        }
-        console.log("Aktuelle Warenkorb Summe: " + warenkorbTotal + "€");
-    }
-})(Aufgabe06 || (Aufgabe06 = {}));
+})(Aufgabe05 || (Aufgabe05 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe06/script.ts
+++ Aufgabe06/script.ts
@@ -1,34 +1,37 @@
- namespace Aufgabe06 {
+namespace Aufgabe05 {
+    artikelErzeugen();
     
-    let aktuelleProdukte: string = "";
-    function artikelErzeugen(süßigkeiten: Süßigkeiten): void {
+    function artikelErzeugen(): void {
+    for (let i: number = 0; i < süßigkeiten.length; i++) {
         
+    
         //Div
         let divCandy: HTMLElement = document.createElement("div");
         divCandy.setAttribute("class", "Produkte");
 
-         //Bild
+        //Bild
         let imgCandy: HTMLElement = document.createElement("img");
-        imgCandy.setAttribute("src", süßigkeiten.img);
-        imgCandy.setAttribute("alt", süßigkeiten.alt);
-
-          //Name
+        imgCandy.setAttribute("src", süßigkeiten[i].img);
+        imgCandy.setAttribute("alt", süßigkeiten[i].alt);
+        
+        //Name
         let candyName: HTMLElement = document.createElement("h2");
-        candyName.innerHTML = süßigkeiten.name;
+        candyName.innerHTML = süßigkeiten[i].name;
 
-         //Preis
+        //Preis
         let candyPreis: HTMLElement = document.createElement("h3");
-        candyPreis.innerHTML = süßigkeiten.preis + "€";
- 
-         //Beschreibung
-        let candybeschreibung: HTMLElement = document.createElement("p");
-        candybeschreibung.innerHTML = süßigkeiten.beschreibung;
+        candyPreis.innerHTML = süßigkeiten[i].preis + "€";
 
-           //Button
+        //Beschreibung
+        let candybeschreibung: HTMLElement = document.createElement("p");
+        candybeschreibung.innerHTML = süßigkeiten[i].beschreibung;
+
+        //Button
         let button: HTMLInputElement = document.createElement("input");
         button.type = "button";
         button.value = "Kaufen";
-       
+        candyPreis.appendChild(button);
+
         // Alle Tags zu div Container
         divCandy.appendChild(imgCandy);
         divCandy.appendChild(candyName);
@@ -36,8 +39,7 @@
         divCandy.appendChild(candybeschreibung);
         divCandy.appendChild(button);
 
-
-        switch (süßigkeiten.kategorie) {
+        switch (süßigkeiten[i].kategorie) {
             case 1:
                 let getContainer1: HTMLElement = document.getElementById("kategorie1")!;
                 getContainer1.appendChild(divCandy);
@@ -49,86 +51,10 @@
                     default:
                         break;
         }
-    }
-    function locationHashChanged() {
-         let produktListe = document.getElementById("kategorie1" + "kategorie2");
-         if (produktListe) {
-                produktListe.innerHTML = ""; }
-    
-            /*if (location.hash) {
-                aktuelleProdukte = location.hash.split("#")[1];
-                if (aktuelleProdukte == "home") {
-                    let artikelUeberschrift: HTMLElement = document.createElement("h2");
-                    artikelUeberschrift.innerHTML = "Süßwaren und Extra Scharf";
-                    document.getElementById("kategorie1" + "kategorie2")?.appendChild(artikelUeberschrift);
-                }
-                else {
-                    let artikelUeberschrift: HTMLElement = document.createElement("h2");
-                    artikelUeberschrift.innerHTML = aktuelleProdukte;
-                    document.getElementById("kategorie1" + "kategorie2")?.appendChild(artikelUeberschrift);
-                }
-            }*/
-        for (let i: number = 0; i < süßigkeiten.length; i++) {
         
-            if (aktuelleProdukte == süßigkeiten[i].kategorie) {
-                    artikelErzeugen(süßigkeiten[i]);
-                }
-                else if (aktuelleProdukte == "home") {
-                    artikelErzeugen(süßigkeiten[i]);
-                }
-
-        } 
     }
 
-    function pageLoad() {
-            
-    location.hash = "startseite";
-    aktuelleProdukte = "startseite";
-            // Artikelliste
-    if (location.hash) {
-                aktuelleProdukte = location.hash.split("#")[1];
-                if (aktuelleProdukte == "startseite") {
-                    let artikelUeberschrift: HTMLElement = document.createElement("h2");
-                    artikelUeberschrift.innerHTML = "Frische Blumen und Vasen";
-                    document.getElementById("artikelListe")?.appendChild(artikelUeberschrift);
-                }
-                else {
-                    let artikelUeberschrift: HTMLElement = document.createElement("h2");
-                    artikelUeberschrift.innerHTML = aktuelleProdukte;
-                    document.getElementById("artikelListe")?.appendChild(artikelUeberschrift);
-                }
-            }
-    for (let i: number = 0; i < süßigkeiten.length; i++) {
-                if (aktuelleProdukte == süßigkeiten[i].kategorien) {
-                    artikelErzeugen(süßigkeiten[i]);
-                }
-                else if (aktuelleProdukte == "home") {
-                    artikelErzeugen(süßigkeiten[i]);
-                }
-            }
-        }
-
-    window.onhashchange = locationHashChanged;  
-    window.onload = pageLoad;
-
-        //let warenkorbTotal: number = 0;
-    let artikelZaehler: number = 0;
-    let warenkorbTotal: number = 0;
-    function handleClick(_event: Event): void  {
-            artikelZaehler ++;
-            if (artikelZaehler >= 0) {
-                const blase = document.getElementById("divBlase");
-                if (blase) blase.innerHTML = "" + artikelZaehler;
-            }
-            let produktId: string = (<HTMLDivElement>(<HTMLElement>_event.currentTarget).parentElement).getAttribute("id")!;
-            
-            
-        // Summe der Preise  
-            for (let i: number = 0; i < süßigkeiten.length; i++) {
-                if (süßigkeiten[i].id == produktId) {
-                    warenkorbTotal = warenkorbTotal + süßigkeiten[i].preis; 
-                }
-            }
-            console.log("Aktuelle Warenkorb Summe: " + warenkorbTotal + "€");  
-        }}
+}
+}
     
+
